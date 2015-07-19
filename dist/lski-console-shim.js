d2827ed8b7ff4457af52ac5f7889bea74f41a37d
Lee <dev@lski.co.uk>
2015-07-19 15:43:59 +0100

    Bumped the version up

--- dist/lski-console-shim.js
+++ dist/lski-console-shim.js
@@ -1,2 +1,2 @@
-/*! lski-console-shim-1.0.1 */
+/*! lski-console-shim-1.0.2 */
 !function(o,n){if(o.console===n){var r=function(){};o.console={log:r,error:r,warn:r,info:r,time:r,timeEnd:r,count:r,debug:r,profile:r,profileEnd:r,trace:r,dir:r,dirxml:r,assert:r,group:r,groupEnd:r}}}(window);
