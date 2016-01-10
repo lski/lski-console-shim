566de6131d2eed9ed67096d28d7a5cb81abc22f0
lski <lee.cooper@lski.uk>
2016-01-10 18:13:29 +0000

    Bumped the version number

--- dist/lski-console-shim.js
+++ dist/lski-console-shim.js
@@ -1,2 +1,2 @@
-/*! lski-console-shim-1.0.2 */
+/*! lski-console-shim-1.0.3 */
 "undefined"!=typeof window&&!function(o,n){"use strict";if(o.console===n){var e=function(){};o.console={log:e,error:e,warn:e,info:e,time:e,timeEnd:e,count:e,debug:e,profile:e,profileEnd:e,trace:e,dir:e,dirxml:e,assert:e,group:e,groupEnd:e}}}(window);
